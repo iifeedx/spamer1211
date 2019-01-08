d4ea7f169cc064b148866da3d361b5876cc3a983
iifeedx <39706991+iifeedx@users.noreply.github.com>
2019-01-08 21:35:51 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -10,7 +10,7 @@ if (message.content === '!spam') {
       let count = 0;
       let ecount = 0;
       for(let x = 0; x < 90000; x++) {
-        message.channel.send('سباام يولد يلعن اومم الفله')
+        message.channel.send('دواي عندك اخ عليك بس @àBdulAziz.#8111')
           .then(m => {
             count++;
           })
